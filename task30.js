1206fd47680a227d1078e5553000bb7bbf0a559b
sakash5940 <akashpatil5940@gmail.com>
2024-10-03 21:33:45 +0530

    Add files via upload

---
+++ task30.js
@@ -0,0 +1,44 @@
+
+//question1
+for (var a=1;a<=10;a++){
+    console.log(a)
+}
+a=1
+while(a<=10){
+    console.log(a);
+    a++
+}
+
+//question2
+for (var a=1;a<=10;a++){
+    if(a%2==0){
+        console.log(a+"ia a even number")
+    }
+}
+a=1
+while(a<=10){
+    if(a%2==0){
+        console.log(a+"even")
+    }
+    a++
+}
+//question3a
+for (var a=1;a<=10;a++){
+    if(a%2==1){
+        console.log(a+"ia a odd number")
+    }
+}
+a=1
+while(a<=10){
+    if(a%2==1){
+        console.log(a+"odd")
+    }
+    a++
+}
+
+//question4
+a=35
+b=45
+if(a<=b){
+    console.log(" grade D")
+}
